54fabf73950715139c1205d82563f401d7afed8b
ouzhehua <ouzhehua@elex-tech.com>
2017-11-02 20:51:28 +0800

    组件：获取UILabel上的URL点击内容，调用回调，回调里使用TextURLOnClick.currentURL获得内容

---
+++ Assets/NGUI/Examples/Scripts/Other/TextURLOnClick.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TextURLOnClick : MonoBehaviour
+{
+    [Serializable]
+    public class URLEventTrigger : UnityEvent { }
+    [SerializeField]
+    private URLEventTrigger onClickURL = new URLEventTrigger();
+
+    /// <summary>
+    /// Use In Call Back Function
+    /// </summary>
+    public static string currentURL;
+    void OnClick()
+    {
+        UILabel lbl = GetComponent<UILabel>();
+
+        if (lbl != null)
+        {
+            string url = lbl.GetUrlAtPosition(UICamera.lastWorldPosition);
+            if (!string.IsNullOrEmpty(url))
+            {
+                if (onClickURL != null && onClickURL.GetPersistentEventCount() > 0)
+                {
+                    currentURL = url;
+                    onClickURL.Invoke();
+                    currentURL = null;
+                }
+            }
+        }
+    }
+}
